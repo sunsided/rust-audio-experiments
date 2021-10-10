12f00e21b4e9f5ca3954ef6084d3efc5fcda2034
Markus Mayer <widemeadows@gmail.com>
2021-10-10 17:21:22 +0200

    Reuse same sink for subsequent tones

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use rodio::{OutputStream, Sink};
+use rodio::{OutputStream, Sink, Source};
 use rodio::source::SineWave;
 use std::{thread, time};
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
     let period = time::Duration::from_millis(1000);
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.set_volume(0.1);
+
     for i in [932, 587, 523, 466].iter() {
-        let sink = Sink::try_new(&stream_handle).unwrap();
-        sink.set_volume(0.1);
-        sink.append(SineWave::new(*i));
-        thread::sleep(period);
-        sink.stop();
+        let source = SineWave::new(*i)
+            .take_duration(period);
+        sink.append(source);
     }
+
+    thread::sleep(4*period);
+    sink.stop();
 }
 
