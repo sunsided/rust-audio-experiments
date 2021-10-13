6db485f63b26e4561b71bd501bc1eb5a209c946b
Markus Mayer <widemeadows@gmail.com>
2021-10-13 19:08:11 +0200

    Move sound generating example to own file

---
+++ examples/rodio_soundgen.rs
@@ -0,0 +1,20 @@
+use rodio::source::SineWave;
+use rodio::{OutputStream, Sink, Source};
+use std::{thread, time};
+
+fn main() {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let period = time::Duration::from_millis(1000);
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.set_volume(0.1);
+
+    for i in [932, 587, 523, 466].iter() {
+        let source = SineWave::new(*i).take_duration(period);
+        sink.append(source);
+    }
+
+    thread::sleep(4 * period);
+    sink.stop();
+}
